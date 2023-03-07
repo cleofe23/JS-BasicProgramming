e45231e496e694c0f815d5e0ed5e93a53750ead6
Cleofe Caballero <cleofecaballero0623@gmail.com>
2023-03-07 19:41:38 +0800

    add bulb lights

---
+++ js/coercion.js
@@ -0,0 +1,14 @@
+let a = 8; // number
+let b = '6'; // string
+b = parseInt(b, 10);
+let c = a + b;
+
+console.log('Answer: ' + c); // COERCION
+
+
+let d = parseInt('Cleo', 10);
+console.log(d); // This should produce the NaN Not a Number
+
+
+let e = isNaN(d);
+console.log(e);
