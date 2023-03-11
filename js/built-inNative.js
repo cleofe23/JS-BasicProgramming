a6a7bcbc7d213acfcfeb95035dfbcd33fb72ee98
Cleofe Caballero <cleofecaballero0623@gmail.com>
2023-03-11 13:34:29 +0800

    add all changes

---
+++ js/built-inNative.js
@@ -0,0 +1,34 @@
+
+/*
+let pattern = /xyz/;
+let value = 'this is xyz a test to replace the value of pattern.';
+console.log(value.replace(pattern, 'hero'));
+console.log(typeof value);
+
+//let myString = new String('howdy');
+//console.log
+
+*/
+
+//let myString = new String('howdy');
+//console.log(myString);
+//console.log(myString.toString());
+//console.log(typeof myString);
+
+
+//String(), number(), Boolean(), Object(), Function(), Symbol()
+
+/*
+let myPrimitive = 'This is PRIMITIVE data type';
+//console.log(typeof myPrimitive);
+myPrimitive = myPrimitive.toLowerCase();
+console.log(typeof myPrimitive);
+*/
+
+
+let myNumber = new Number(10);
+console.log(typeof myNumber);
+
+let myPrimitive2 = myNumber.valueOf();
+console.log(myPrimitive2);
+console.log(typeof myPrimitive2);
