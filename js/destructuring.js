7a77ee2c3034dd61eb6a8e03ee4a0fcf196d5fe2
Cleofe Caballero <cleofecaballero0623@gmail.com>
2023-03-11 11:30:58 +0800

    add destructuring.js

--- js/destructuring.js
+++ js/destructuring.js
@@ -1,11 +1,31 @@
+
+
 let a,b,c,d,e;
 
 let names = ['cleo', 'mark', 'laine', 'luffy', 'jhon'];
 
 [a,b,c,d,e] = names;
-
+/*
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
 console.log(e);
+*/
+/*
+let others;
+[a,b, ...others] = names;
+console.log(a);
+console.log(b);
+console.log(others);
+*/
+let year, model;
+({year,model} = {
+    make: 'Porche',
+    model: '9R',
+    year:2021,
+    value:5000,
+});
+
+console.log(year);
+console.log(model);
